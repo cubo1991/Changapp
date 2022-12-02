411074751ff1b2fe9b640fec2c1334d2707e52cd
Neovhack <nnnicoboland@hotmail.com>
2022-12-02 18:24:43 -0300

    Set de redux

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+         
           Learn React
         </a>
       </header>
---
+++ Front-End/src/actions/index.js
@@ -0,0 +1,8 @@
+/* export const TEST = "TEST"
+
+export function test(){
+    return {
+        type: TEST,
+        payload: "salio ok"
+    }
+} */
