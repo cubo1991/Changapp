6cd1a026b992947ff09dc7045ce327cfe7898645
FrancoRumiky <framqoo@gmail.com>
2022-12-03 11:02:25 -0300

    add try-catch suppliersSeacher

--- Back-End/src/middlewares/suppliersSearcher.js
+++ Back-End/src/middlewares/suppliersSearcher.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const { Supplier, Op } = require("../db.js");
 
-router.get("/", async (req, res) => {
+try {
   const { name } = req.query;
   if (name) {
     //http://localhost:3001/suppliers?name=Techos
@@ -20,26 +20,26 @@ router.get("/", async (req, res) => {
     const result = await Supplier.findAll();
     res.send(result);
   }
-});
-
+} catch (error) {
+  next(error);
+}
 
 router.post("/", async (req, res) => {
   const { name, cuit, description } = req.body;
-  if(name,cuit,description){
-  try {
-    await Supplier.create({
-      name,
-      cuit,
-      description,
-    });
-    res.json("Supplier created successful")
-  } catch (e) {
-    res.status(500).json("error in data provided")
+  if ((name, cuit, description)) {
+    try {
+      await Supplier.create({
+        name,
+        cuit,
+        description,
+      });
+      res.json("Supplier created successful");
+    } catch (e) {
+      res.status(500).json("error in data provided");
+    }
+  } else {
+    res.status(404).json("Missing data");
   }
-} else{
-  res.status(404).json("Missing data")
-}
 });
-
 
 module.exports = router;
