b8a4fabd83d947a2b7c1fa22e24e7abeae468605
unknown <you@example.com>
2022-12-03 09:56:10 -0300

    add post suppliers in supplierSeracher.js

--- Back-End/src/middlewares/suppliersSearcher.js
+++ Back-End/src/middlewares/suppliersSearcher.js
@@ -3,23 +3,41 @@ const router = Router();
 const { Supplier, Op } = require("../db.js");
 
 router.get("/", async (req, res) => {
-    const {name} = req.query
+  const { name } = req.query;
   if (name) {
     //http://localhost:3001/suppliers?name=Techos
     const result = await Supplier.findAll({
-        where: {
-            name : {
-                [Op.substring]: name
-            }
-        }
+      where: {
+        name: {
+          [Op.substring]: name,
+        },
+      },
     });
-    if(result.length === 0 ) res.send("No hubo resultados para la busqueda");
+    if (result.length === 0) res.send("No hubo resultados para la busqueda");
     else res.send(result);
   } else {
     //http://localhost:3001/suppliers
     const result = await Supplier.findAll();
     res.send(result);
   }
+});
+
+router.post("/", async (req, res) => {
+  const { name, cuit, description } = req.body;
+  if(name,cuit,description){
+  try {
+    await Supplier.create({
+      name,
+      cuit,
+      description,
+    });
+    res.json("Supplier created successful")
+  } catch (e) {
+    res.status(500).json("error in data provided")
+  }
+} else{
+  res.status(404).json("Missing data")
+}
 });
 
 module.exports = router;
