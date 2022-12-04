8eea591a58ee4ba39f82bc24b6234d146e4f88fe
Luis Marcano <lmarcano203@gmail.com>
2022-12-03 19:53:26 -0500

    feature(add:ServicesOrderByPrice / mod:Services.js->Se agrego a la ruta el middle del ordernamiento por precio )

---
+++ Back-End/src/middlewares/ServicesOrderByPrice.js
@@ -0,0 +1,65 @@
+const { Service, Op } = require('../db.js');
+const { Supplier } = require('../db.js');
+
+const { Router } = require('express');
+const router = Router();
+
+module.exports = router.get('/', async (req, res, next) => {
+
+  try{
+
+    let { filter, order } = req.query;
+
+    //filter => recibe un string con los services que se esten mostrando en caso de estar filtrados
+    //order => "ASC" para ordenamiento ascendente y "DESC" para ordenamiento descendente
+
+    // http://localhost:3000/services?filter=Limpieza%20de%20Hoteles,Limpieza%20de%20Hogares&order=ASC
+    if(filter && filter !== "undefined"){
+
+      filter = filter.split(",");
+
+      console.log(filter)
+      
+      const OrderBy = await Service.findAll({
+        where: {
+          serviceType: {
+            [Op.any] : filter
+          }
+        },
+        include: [
+          {
+            model: Supplier
+          }
+        ],
+        order: [
+          ['pricePerHour', order]
+        ]
+      });
+
+      res.status(200).json(OrderBy);
+
+    } else if(filter === "undefined") {   // http://localhost:3000/services?filter=undefined&order=ASC
+
+      const OrderBy = await Service.findAll({
+
+        include: [
+          {
+          model: Supplier
+          }
+      ],
+        order: [
+          ['pricePerHour', order]
+        ]
+      });
+
+      res.status(200).json(OrderBy);
+    
+    }else next()
+
+    
+    
+
+  }catch(error){
+    next(error)
+  }
+});
--- Back-End/src/routes/services.js
+++ Back-End/src/routes/services.js
@@ -3,8 +3,12 @@ const { Router } = require('express');
 
 const router = Router();
 
+//Ruta /services?filter=undefined&type=ASC(ó DESC) => para ordenar todos los services por pricePerHour ASC o DESC
+//Ruta /services?filter=(string con services separados por coma)&type=ASC(ó DESC) => para ordenar los servicios que se manden desde el front en caso de estar filtrados
+router.use(require('../middlewares/ServicesOrderByPrice.js'));
 
-
+//Ruta /services?type="limp" => para buscar services por match total o parcial junto con sus suppliers
+//Ruta /services para traer todos los services con sus suppliers
 router.use(require('../middlewares/servicesSearch.js'));
 
 
