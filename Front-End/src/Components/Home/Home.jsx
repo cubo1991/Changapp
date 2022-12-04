3f9478af30210e2af81b5deadaaab6dd6d04ca25
FrancoRumiky <framqoo@gmail.com>
2022-12-04 17:03:22 -0300

    add routes app.js - Home - modified navbar

--- Front-End/src/Components/Home/Home.jsx
+++ Front-End/src/Components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Services } from '../Servicios/Services.jsx'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Services />
+    </>
+
   )
 }
