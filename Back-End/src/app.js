262f9a3ca9510566f7392b0103ee9e185fa7d9da
Luis Marcano <lmarcano203@gmail.com>
2022-12-05 03:56:25 -0500

    fix: correccion de error de sintaxis en app.js que hacia un bloque de cors

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -17,7 +17,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http//localhost3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
