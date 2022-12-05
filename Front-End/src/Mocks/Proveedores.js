a840493f9d1046a82c3ee48b294b5837670c4af3
cubo1991 <adavidlopezmathez@gmail.com>
2022-12-05 14:24:56 -0300

    Logica de Suppliers Detail

---
+++ Front-End/src/Mocks/Proveedores.js
@@ -0,0 +1,26 @@
+export const Proveedores = [
+    {
+        "name": "Limpieza S.A.",
+        "cuit": 305321450,
+        "description": "Realiza Limpieza del hogar",
+        "id":1,
+    },
+    {
+        "name": "Paredes S.A.",
+        "cuit": 3012412340,
+        "description": "Repara Paredes",
+        "id":2,
+    },
+    {
+        "name": "Cloacas S.A.",
+        "cuit": 3012412340,
+        "description": "Reparacion y limpieza de Cloacas",
+        "id":3,
+    },
+    {
+        "name": "Techos S.A.",
+        "cuit": 3012412340,
+        "description": "Repara Techos",
+        "id":4,
+    }
+]
---
+++ Front-End/src/Mocks/Servicios.js
@@ -0,0 +1,32 @@
+export const Servicios = [
+    {
+        "serviceType": "Reparacion de Techos",
+        "pricePerHour": "30000",
+        "description": "Realiza Limpieza del hogar"
+    },
+    {
+        "serviceType": "Reparacion de Paredes",
+        "pricePerHour": "25000",
+        "description": "Realiza Limpieza del hogar"
+    },
+    {
+        "serviceType": "Limpieza de Hogares",
+        "pricePerHour": "10000",
+        "description": "Realiza Limpieza del hogar"
+    },
+    {
+        "serviceType": "Limpieza de Hospitales",
+        "pricePerHour": "15000",
+        "description": "Realiza Limpieza del hogar"
+    },
+    {
+        "serviceType": "Limpieza de Cloacas",
+        "pricePerHour": "8000",
+        "description": "Realiza Limpieza del hogar"
+    },
+    {
+        "serviceType": "Reparacion de Cloacas",
+        "pricePerHour": "12000",
+        "description": "Realiza Limpieza del hogar"
+    }
+]
