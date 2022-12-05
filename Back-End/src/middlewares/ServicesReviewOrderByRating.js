fa8b42c19bce4a6ab421b9266cbf2dbc9ef64ec2
Neovhack <nnnicoboland@hotmail.com>
2022-12-05 18:24:58 -0300

    agregar promedio de rating en proveedores
    Agregar Servicios a detalles del proveedor
    
    Ordenamiento por rating (Proveedores)

--- Back-End/src/middlewares/ServicesReviewOrderByRating.js
+++ Back-End/src/middlewares/ServicesReviewOrderByRating.js
@@ -3,13 +3,13 @@ const { Router } = require('express');
 
 const router = Router();
 
-module.exports = router.get('/order', async (req, res, next) => {
+module.exports = router.get('/orderService', async (req, res, next) => {
 
   try{
 
     let {dir, filter} = req.query;
 
-  /* http://localhost:3000/review/order?dir=ASC (ASC para orden ascendente, DESC para descendente) =>
+  /* http://localhost:3000/review/orderService?dir=ASC (ASC para orden ascendente, DESC para descendente) =>
      Devuelvfe todos los servicios en la base de datos ordenados por su rating, dependiendo del dir valor que recibe por dir*/
     if(dir && !filter && (dir === "ASC" || dir === "DESC")){
 
@@ -21,7 +21,7 @@ module.exports = router.get('/order', async (req, res, next) => {
       })
       return res.status(200).json(result)
 
-    }else if (filter && (dir === "ASC" || dir === "DESC")){   // http://localhost:3000/review/order?filter=Limpieza de Hospitales&dir=ASC
+    }else if (filter && (dir === "ASC" || dir === "DESC")){   // http://localhost:3000/review/orderService?filter=Limpieza de Hospitales&dir=ASC
                                                               // Devulve solo los servicios donde el serviceType se pasa por filter (en caso de que esten filtrados) y los ordena dependiendo del valor de dir
       const result = await Review.findAll({
         order:[
--- Back-End/src/routes/suppliers.js
+++ Back-End/src/routes/suppliers.js
@@ -3,8 +3,9 @@ const { Router } = require('express');
 
 const router = Router();
 
-
+router.use(require("../middlewares/SupplierReviewOrderByRating"))
 router.use(require("../middlewares/suppliersSearcher"))
+
 
 
 module.exports = router;
