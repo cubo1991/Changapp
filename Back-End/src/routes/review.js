dc29ee273f85b26667789494a7f2fb8f25017665
Luis Marcano <lmarcano203@gmail.com>
2022-12-05 01:18:02 -0500

    features(add:serviceReviewOrderByRating.js // mod: routes/review.js => se agrego la ruta de servicesReviewOrderByRating)

--- Back-End/src/routes/review.js
+++ Back-End/src/routes/review.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.use(require('../middlewares/ServicesReviewOrderByRating.js'));
 
 router.use(require('../middlewares/reviewLoad'));
 
