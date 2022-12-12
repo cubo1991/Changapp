5aea86a40cdc45d28bf546f640a89140c90c16da
Neovhack <nnnicoboland@hotmail.com>
2022-12-12 17:49:08 -0300

    Ruta con info de usuario (login)

--- Back-End/src/middlewares/login.js
+++ Back-End/src/middlewares/login.js
@@ -3,7 +3,21 @@ const { Router } = require('express');
 const router = Router();
 
 
-module.exports = router.get('/', async (req, res) => {
+module.exports = router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    let userDB = await User.findAll({
+      where: {
+        id: id
+      }
+    })
+    res.status(200).json(userDB)
+  } catch (e) {
+    console.log(e.message || e)
+    res
+        .status(404)
+        .json(e.message || e)
+  }
   res.send("hola")
 
 });
