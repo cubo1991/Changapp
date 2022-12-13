327f9b123cc94d0bf91ca42209f902b7cc7c4bab
Luis Marcano <lmarcano203@gmail.com>
2022-12-13 08:53:46 -0500

    Featues(add:postService / mod: reviewLoad para que busque el id del servicio correctamente / mod:reviewForm para que envie el post correctamente / mod: SuppliersDetails para que muestre el reviewF solo si se esta logeado

--- Back-End/src/routes/services.js
+++ Back-End/src/routes/services.js
@@ -11,6 +11,7 @@ router.use(require('../middlewares/ServicesOrderByPrice.js'));
 
 //Ruta /services?filter=undefined&type=ASC(ó DESC) => para ordenar todos los services por pricePerHour ASC o DESC
 //Ruta /services?filter=(string con services separados por coma)&type=ASC(ó DESC) => para ordenar los servicios que se manden desde el front en caso de estar filtrados
+router.use(require('../middlewares/postService.js'))
 router.use(require('../middlewares/servicesSearch.js'));
 
 module.exports = router;
