961bf460fbd375f98d4292a2de1e05ad5ce8f5ba
FacundoIsoler <facundoisoler@outlook.com>
2023-01-05 19:05:29 -0300

    Protección rol de Supplier

--- Front-End/src/Components/NavBar/NavBar.jsx
+++ Front-End/src/Components/NavBar/NavBar.jsx
@@ -47,11 +47,11 @@ export default function NavBar() {
                   Contacto
                 </a>
               </li>
-              {userRole === "Supplier"? <li>
+              {userRole === "Supplier" ? <li>
                 <a className="dropdown-item" href="/suppliersContact">
                   Publicá tu servicio
                 </a>
-              </li>: ""}   
+              </li> : ""}
             </ul>
           </div>
           <NavLink
@@ -75,44 +75,46 @@ export default function NavBar() {
             </div>
           </NavLink>
 
-            {
-               userRole === "Supplier"?
-              <NavBarSupplier/>
+          {
+            userRole === "Supplier" ?
+              <NavBarSupplier />
               : ""
-            }
+          }
 
           <div className={`${Style.nav_right} d-flex justify-content-end col`}>
             <Searchbar />
             <Login></Login>
+            {isAuthenticated && user.user_role !== "Supplier" ?
+              <NavLink
+                to="/cart"
+                className={({ isActive }) =>
+                  isActive ? Style.active : Style.inactive
+                }
+              >
+                <div className={Style.cart}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="25"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                  </svg>
 
-            <NavLink
-              to="/cart"
-              className={({ isActive }) =>
-                isActive ? Style.active : Style.inactive
-              }
-            >
-              <div className={Style.cart}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                </svg>
-              
-            
-            {
-            cart.length > 0 
-            ?
-            <p className="btn btn-danger" >{cart.length}</p>
-            :
-            ""
 
+                  {
+                    cart.length > 0
+                      ?
+                      <p className="btn btn-danger" >{cart.length}</p>
+                      :
+                      ""
+
+                  }
+                </div>
+              </NavLink>
+              : null
             }
-            </div>
-            </NavLink>
           </div>
         </div>
       </nav>
