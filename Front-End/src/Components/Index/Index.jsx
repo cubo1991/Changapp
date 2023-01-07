bc9f7e8241a0bf82a7c3f2a43e1fe6fa18ede89b
Luis Marcano <lmarcano203@gmail.com>
2023-01-07 16:42:43 -0500

    features(fix: se acomodo el paginado para que no generará numeración indefinida a medida que aumenta de paginas )

--- Front-End/src/Components/Index/Index.jsx
+++ Front-End/src/Components/Index/Index.jsx
@@ -18,6 +18,13 @@ export default function Paginado({ servicesPerPage, allServices, index, currentP
         pageNumber.push(i + 1)
     }
 
+    let barSize = [...pageNumber];
+    if(pageNumber.length > 5){
+        barSize = barSize.slice(2, pageNumber.length-2);
+    }
+    console.log(barSize, "BARSIZE")
+    console.log(pageNumber.length)
+
     return (
         <div>
             <div className={s.pagination}>
@@ -25,10 +32,34 @@ export default function Paginado({ servicesPerPage, allServices, index, currentP
                     <ul className="pagination">
                         <button className={`page-link ${s.buttons}`} onClick={(e) => nextPrevButton(e)} value="prev">Anterior</button>
                         {pageNumber &&
-                            pageNumber.map(number => (
-                                <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
-                                    onClick={() => index(number)}> {number}</p></li>
-                                )
+                            pageNumber.map(number => {
+                                return <>
+                                {pageNumber.length < 5 ? <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
+                                    onClick={() => index(number)}> {number}</p></li> : null}
+
+                                {pageNumber.length > 5 && number < 3 ? <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
+                                    onClick={() => index(number)}> {number}</p></li> : null}
+                                    
+                                    {pageNumber.length > 5 && number === 2 ? 
+                                    <>
+                                    <li className="page-item" ><p className={ `page-link`} key={number} value={number}
+                                     >...</p></li>
+                                    {currentPage > 2 && currentPage < pageNumber.length -1 ? <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
+                                    onClick={() => index(currentPage)}> {currentPage}</p></li> : null }
+                                    </> : null}
+
+                                    {/* {pageNumber.length > 5 && number > 1 && number < pageNumber.length - 1 ? <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
+                                    onClick={() => index(number)}> {number}</p></li>: null } */}
+                                    
+
+                                    {pageNumber.length > 5 && number === pageNumber.length - 1 ? <li className="page-item" ><p className={ `page-link`} key={number} value={number}
+                                        >...</p></li> : null}
+                        
+                                    {pageNumber.length > 5 && number > pageNumber.length - 2 ? <li className="page-item" ><p className={ currentPage === number ? `page-link ${s.activePage}` : `page-link ${s.focusPage}`} key={number} value={number}
+                                    onClick={() => index(number)}> {number}</p></li> : null}
+                                    </>
+
+                                } 
                             )}
                             {/* <li className="page-item" ><p className="page-link" key={number} value={number}
                                     onClick={() => index(number)}> {number}</p></li> */}
