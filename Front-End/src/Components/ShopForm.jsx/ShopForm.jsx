8b1fdf311ef4e6979ee525350794f8a47054e40e
NadiaO089 <nadiaolivera@gmail.com>
2023-01-09 15:59:13 -0300

    Formulario de compra corregido

--- Front-End/src/Components/ShopForm.jsx/ShopForm.jsx
+++ Front-End/src/Components/ShopForm.jsx/ShopForm.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {useSelector, useDispatch } from 'react-redux';
+import { useAuth0 } from "@auth0/auth0-react";
+import { getUserDetails} from '../../actions';
 import MercadoPagoProduct from '../MercadoPagoProduct/MercadoPagoProduct';
 import s from '../ShopForm.jsx/ShopForm.module.css'
 
 
 export const ShopForm = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart)
 
-    const inicialState = {
-        name: '',
-        lastname: '',
-        phone: '',
-        email: '',
-        adress:'',
-        location:'',
-        CP: ''
-    }
+  const { user } = useAuth0();
 
-    
+useEffect(()=> {
+  if(user) dispatch(getUserDetails(user.id), true)
+},[dispatch, user]);
+
+  const userDetails = useSelector(state => state.userDetails)
+
+  var inicialState = {};
+
+  if(userDetails.length) {
+    inicialState = {
+      name: userDetails[0].name && userDetails[0].family_name? `${userDetails[0].name} ${userDetails[0].family_name}` : '*',
+      phone: userDetails[0].Detail.phoneNumber? userDetails[0].Detail.phoneNumber : '*',
+      email: userDetails[0].email? userDetails[0].email : '*',
+      adress: userDetails[0].Detail.adress? userDetails[0].Detail.adress : '*',
+      location: userDetails[0].Detail.location? userDetails[0].Detail.location : '*',
+      CP: '',
+      preferredTime:''
+  }
+  } else {
+    inicialState = {
+      name: user.name? user.name : '*',
+      phone: '*',
+      email: user.email? user.email : '*',
+      adress: '*',
+      location: '*',
+      CP: '',
+      preferredTime:''
+  }
+  }
+ 
     const [input, setInput] = useState(inicialState);
     const [pagar, setPagar] = useState(false);
 
@@ -71,39 +95,6 @@ export const ShopForm = () => {
           }
         });
       }
-
-      function lastnameValidator (lastname, set) {
-        if(!/^\S/m.test(lastname)){
-          set(prev => {     
-            return {
-              ...prev,
-              lastnameError: "Este campo no puede empezar con un espacio vacío"
-            }    
-          });
-        }
-        else if(!/\S$/gm.test(lastname)){
-          set(prev => {     
-            return {
-              ...prev,
-              lastnameError: "Este campo no puede terminar con un espacio en vacío"
-            }    
-          });
-        }
-        else if( /[^A-Za-zÑ-ñ- ]/.test(lastname) /* ||  */){
-          set(prev => {     
-            return {
-              ...prev,
-              lastnameError: "El nombre no puede contener carácteres especiales"
-            }    
-          });
-        } 
-        else set( prev => {
-          return {
-            ...prev,
-            lastnameError: ""
-          }
-        });
-      }
   
       function emptyValidator (name, set, event){
   
@@ -153,33 +144,64 @@ export const ShopForm = () => {
         });
     }
 
+    const handlerClick = (e) => {
+      setInput({
+        ...input,
+        [e.target.name]:e.target.value
+      });
+    }
 //
     const handlerSubmit = (e) => {
         e.preventDefault();
+        console.log(input)
         setPagar(true);
+ 
 
     }
 
-    console.log("Rendering ShopForm");
-
+    //console.log("Rendering ShopForm");
+    let i = 1;
+    let total = 0;
+  
     return (
 
         <div className={s.container}>
-            <h2 className="h2">Datos del comprador</h2>
+            <h2 className="h2">Finaliza tu Compra</h2>
 
             <div className='card' style={{ width: "40rem", left: "22rem", top: "2rem" }}>
-            
+            <div className={s.itemsContainer}>
+              <h3>Tu compra</h3>
+          <ul className={s.ul}>
+            {cart.map(c => {
+              return (
+                <div>
+                <li>
+                <div className={s.order}><p class="mb-1">Ítem #{i}: </p><h6 class="mb-1"> {c.serviceType}</h6></div>
+                  <div className={s.order}><p class="mb-1">Precio por Unidad: </p><h6 class="mb-1"> ${c.pricePerHour}</h6></div>
+                  <div className={s.order}><p class="mb-1">Turnos reservados: </p><h6 class="mb-1"> {c.amount}</h6></div>
+                  <div className={s.order}><p class="mb-1">Precio Total: </p><h6 class="mb-1"> ${c.pricePerHour*c.amount}</h6></div>
+                  <p hidden='true'>{total = total + c.pricePerHour*c.amount}{i++}</p>
+                  <div className={s.linea}></div>
+                </li></div>
+              )
+            })}
+            <h6 className={s.total}>Total: ${total}</h6>
+          </ul>
+
+            </div>
+            <div className={s.linea}></div>
+
             <form className="row g-3" onSubmit={(e)=>handlerSubmit(e)}>
 
                 <div class="form-group">
-
+            <h3>Datos del Comprador</h3>
                     <div div className="col-12">
-                    <label>Nombres: </label>
+                    <label>Nombre y Apellido:</label>
                     <input
                         type='text'
                         name='name'
                         class="form-control"
-                        placeholder='*Required data'
+                        placeholder='*'
                         value={input.name}
                         onChange={(e) => {
                             onChange(e);
@@ -187,28 +209,15 @@ export const ShopForm = () => {
                      <br></br>
                      {error.nameError && <span className={s.error}>{error.nameError}</span>}
                      </div>
-                        
-                    <div div className="col-12">
-                    <label>Apellidos: </label>
-                    <input
-                        type='text'
-                        name='lastname'
-                        class="form-control"
-                        placeholder='*Required data'
-                        value={input.lastname}
-                        onChange={(e) => {
-                            onChange(e);
-                            lastnameValidator(e.target.value, setError)}} />
-                     {error.lastnameError && <span className={s.error}>{error.lastnameError}</span>}
-                    </div>
                     
-                    <div className="col-md-6">
+                  <div class="row">
+            <div div class="col">
                     <label>E-mail: </label>
                     <input
                         type="email"
                         class="form-control"
                         name='email'
-                        placeholder='*Required data'
+                        placeholder='*'
                         value={input.email}
                         onChange={(e) => {
                             onChange(e)
@@ -218,41 +227,44 @@ export const ShopForm = () => {
                       {error.emailError && <span className={s.error}>{error.emailError}</span>}
                     </div>
 
-                    <div className="col-md-6">
+                    <div div class="col">
                     <label>Número de Teléfono: </label>
                     <input
                         type='number'
                         name='phone'
                         class="form-control"
-                        placeholder='*Required data'
+                        placeholder='*'
                         value={input.phone}
                         onChange={(e) => onChange(e)} />
                     
                     </div>
+                    </div>
+        <div className={s.linea}></div>
                     
                     <div className="col-12">
+                    <h3>Datos de entrega</h3>
                     <label>Dirección</label>
                     <input
                         type='text'
                         name='adress'
                         class="form-control"
-                        placeholder='*Required data'
-                        value={input.street}
+                        placeholder='*'
+                        value={input.adress}
                         onChange={(e) => {
                             onChange(e);
                             emptyValidator(e.target.value, setError, "addressError")
                         }} />
                         {error.addressError && <span className={s.error}>{error.addressError}</span>}
                     </div>
-
+                  <div class="row">
                     <div className="col-9">
                     <label>Ciudad: </label>
                     <input
                         type='text'
                         name='location'
                         class="form-control"
-                        placeholder='*Required data'
-                        value={input.ciudad}
+                        placeholder='*'
+                        value={input.location}
                         onChange={(e) => {
                             onChange(e);
                             emptyValidator(e.target.value, setError, "locationError")
@@ -266,22 +278,32 @@ export const ShopForm = () => {
                         type='number'
                         name='CP'
                         class="form-control"
-                        placeholder='*Required data'
+                        placeholder='*'
                         value={input.CP}
                         onChange={(e) => onChange(e)} />
                     </div>
-
+                    <div className={s.linea}></div>
+</div>
+                    <div className="col-12">
+                    <h3>Horario de Preferencia</h3>
+                    <select class="form-control" name='preferredTime' onClick={(e) => handlerClick(e)}>
+                    <option>Mañana (entre las 8:00 y las 12:00)</option>
+                    <option>Siesta (entre las 12:00 y las 16:00)</option>
+                    <option>Tarde (entre las 16:00 y las 20:00)</option>
+                    </select>
+                          
+                </div>
+<br></br>  <p className={s.error}>*Son datos requeridos</p>
                     <div className="col-md-12">
                     <input
                         class="btn btn-primary"
                         value='Solicitar Pago'
                         type='submit'
                         disabled= {
-                            error.nameError || input.name === "" || error.lastnameError || input.lastname === "" || error.emailError || error.locationError ||
-                            error.addressError || error.locationError ? true : false
+                            error.nameError || input.name === "*" || input.name === "" || error.emailError || input.email === "*" || input.email === ""|| error.locationError || input.location === "*" || input.location === ""||
+                            error.addressError || input.adress === "*" || input.adress === ""|| error.phoneError || input.phone === "*" || input.phone === ""? true : false
                           }
                     />
-
                     <div>
                     {pagar? <MercadoPagoProduct/> : null}
                     </div>
